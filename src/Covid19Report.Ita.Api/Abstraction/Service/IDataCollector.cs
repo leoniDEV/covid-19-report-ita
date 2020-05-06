055ceb95babe21b81c3c3253778f9b33f0ea5af4
leoniDEV <leonidev@outlook.com>
2020-05-06 08:27:32 +0200

    implementato ed adottato parser CSV

--- src/Covid19Report.Ita.Api/Abstraction/Service/IDataCollector.cs
+++ src/Covid19Report.Ita.Api/Abstraction/Service/IDataCollector.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 using Covid19Report.Ita.Api.Infrastructure;
@@ -9,6 +7,6 @@ namespace Covid19Report.Ita.Api.Abstraction.Service
 {
     public interface IDataCollector
     {
-        Task<T> GetDataAsync<T>(string url, SerializerKind serializerKind, JsonSerializerOptions? jsonSerializerOptions = null);
+        IAsyncEnumerable<T> GetDataAsync<T>(string url, SerializerKind serializerKind);
     }
 }
--- src/Covid19Report.Ita.Api/Abstraction/Service/IDataCollectorSerializer.cs
+++ src/Covid19Report.Ita.Api/Abstraction/Service/IDataCollectorSerializer.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Covid19Report.Ita.Api.Infrastructure;
 
@@ -10,7 +8,8 @@ namespace Covid19Report.Ita.Api.Abstraction
     public interface IDataCollectorSerializer
     {
         SerializerKind SerializerKind { get; }
-        Task<T> GetDataAsync<T>(string data, JsonSerializerOptions? jsonSerializerOptions = null);
-        Task<T> GetDataAsync<T>(Stream data, JsonSerializerOptions? jsonSerializerOptions = null);
+
+        IAsyncEnumerable<T> GetDataAsync<T>(string data);
+        IAsyncEnumerable<T> GetDataAsync<T>(Stream data);
     }
 }
